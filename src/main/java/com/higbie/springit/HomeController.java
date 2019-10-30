e835ebe59f62f0504e02466c71b5a1a3080a30ad
ghigbie <gehigbie@gmail.com>
2019-10-29 19:57:54 -0700

    Add home controller

---
+++ src/main/java/com/higbie/springit/HomeController.java
@@ -0,0 +1,15 @@
+package com.higbie.springit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Yo baby!";
+    }
+}
+
+
