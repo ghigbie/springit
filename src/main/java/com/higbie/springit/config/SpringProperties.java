b9d7c7d3a6f829c7b8909f21d51f007009bd51b5
ghigbie <gehigbie@gmail.com>
2019-10-29 20:07:06 -0700

    Add spring properties class

---
+++ src/main/java/com/higbie/springit/config/SpringProperties.java
@@ -0,0 +1,15 @@
+package com.higbie.springit.config;
+
+public class SpringProperties {
+    private String welcomeMsg = "Yo default message!";
+
+    public String getWelcomeMsg() {
+        return welcomeMsg;
+    }
+
+    public void setWelcomeMsg(String welcomeMsg) {
+        this.welcomeMsg = welcomeMsg;
+    }
+
+
+}
