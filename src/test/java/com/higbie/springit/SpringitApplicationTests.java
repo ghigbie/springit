aef57fa045865d75b1f207006f5105ce996dc3f3
ghigbie <gehigbie@gmail.com>
2019-10-29 19:49:00 -0700

    Initial commit

---
+++ src/test/java/com/higbie/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.higbie.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
