3d5ba216df804b534a88e6a3986898b94a5551ff
ghigbie <gehigbie@gmail.com>
2019-11-05 20:34:31 -0700

    Make lombok changes

--- src/main/java/com/higbie/springit/domain/Link.java
+++ src/main/java/com/higbie/springit/domain/Link.java
@@ -1,10 +1,15 @@
 package com.higbie.springit.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
+@Data
 public class Link {
 
     @Id
@@ -12,32 +17,6 @@ public class Link {
     private Long id;
     private String title;
     private String url;
-
-    public Link() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
 
     @Override
     public String toString() {
